ff822c79255b4f5a55b70f92937d250d66291913
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-09-08 21:59:19 +0530

    Fix duplicate entry in background images array

--- landing-page-starter/script.js
+++ landing-page-starter/script.js
@@ -3,7 +3,7 @@ let nextBtn = document.querySelector('.next')
 let prevBtn = document.querySelector('.prev')
                                           
 let BgImgs = [
-    "https://tripandtales.com/wp-content/uploads/2025/08/Miramar-Beach-1024x576.jpg",  
+    "https://tripandtales.com/wp-content/uploads/2025/08/Miramar-Beach-1024x576.jpg",               
     "https://aazhimalasivatemple.in/wp-content/uploads/2024/11/cropped-tea-banner-1536x922-1.jpg",
     "https://api.trekpanda.in/uploads/river%20mountains%20meghalaya.jpg",
     "https://amazemytrip.com/wp-content/uploads/2019/02/Pamban-Bridge.jpg"
